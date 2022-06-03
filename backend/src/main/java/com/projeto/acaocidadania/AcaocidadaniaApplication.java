a12fa1cf1aade704103f23ffb1c81cc18a3844dc
LuizFelipeGatto <luizfelipegatto08@gmail.com>
2022-06-03 19:37:48 -0300

    subindo backend

---
+++ backend/src/main/java/com/projeto/acaocidadania/AcaocidadaniaApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.acaocidadania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcaocidadaniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcaocidadaniaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/projeto/acaocidadania/AcaocidadaniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.acaocidadania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcaocidadaniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
